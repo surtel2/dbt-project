e074b29fedc833070f61ae9bcb43092dad3e5783
Alexander Zlygostev <letrus.alex@gmail.com>
2026-09-06 04:46:18 +0000

    generate models

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,12 +1,24 @@
+with 
 
-SELECT
-    id,
-    orderid,
-    paymentmethod,
-    status,
-    amount,
-    created,
-    _batched_at
-FROM {{ source('stripe', 'payment') }}
+source as (
 
+    select * from {{ source('stripe', 'payment') }}
 
+),
+
+renamed as (
+
+    select
+        id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
