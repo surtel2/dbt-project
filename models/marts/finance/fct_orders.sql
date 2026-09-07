53e09a29bc29280c144583ac183847b7b31d4d2e
Alexander Zlygostev <letrus.alex@gmail.com>
2026-09-07 03:17:01 +0000

    names fixes

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -5,6 +5,6 @@ SELECT
 
 FROM {{ ref('stg_jaffle_shop__orders') }} as o
     LEFT JOIN {{ ref('stg_stripe__payment') }} as p
-        ON o.order_id = p.orderid AND p.status = 'success'
+        ON o.order_id = p.order_id AND p.payment_status = 'success'
 GROUP BY
     o.order_id
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -9,12 +9,12 @@ source as (
 renamed as (
 
     select
-        id,
-        orderid,
-        paymentmethod,
-        status,
-        amount,
-        created,
+        id              as payment_id,
+        orderid         as order_id,
+        paymentmethod   as payment_method,
+        status          as payment_status,
+        amount / 100    as amount, --stored in cents, convert it in dollards
+        created         as created_at,
         _batched_at
 
     from source
